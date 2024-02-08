42705b26ab7023eec8a59bfb872ff98836bca974
manti66 <122206959+Mantarov666@users.noreply.github.com>
2024-02-08 16:50:16 +0200

    Static and CSS ready

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const port = 5000;
 
 const handlebars = require('express-handlebars');
 
+
 app.engine('hbs', handlebars.engine({extname:'hbs'}));
 app.set('view engine', 'hbs');
-
+app.use(express.static('src/static'));
 
 app.get('/', (req, res)=>{
     res.render('home')
