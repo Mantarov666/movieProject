e56bf9944c9356ff152cafb3adea99d9665619cc
manti66 <122206959+Mantarov666@users.noreply.github.com>
2024-02-08 16:21:14 +0200

    First HTML steps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const path =require('path')
 const port = 5000; 
 
+const handlebars = require('express-handlebars');
 
+app.engine('hbs', handlebars.engine({extname:'hbs'}));
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
 
 
 app.listen( port, ()=>{
